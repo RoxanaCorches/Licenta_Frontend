77c6c6177e1b35602fcea4e73d770f61edb173f2
RoxanaCorches <roxanacorches032@gmail.com>
2026-01-18 15:47:37 +0200

    optiunile care apar la your account

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { FaWallet, FaBuildingCircleArrowRight } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { FaSuitcase } from "react-icons/fa";
+import { RiLogoutCircleRFill } from "react-icons/ri";
 import "../pages/Navbar.css";
 import { useWallet } from "../hooks/WalletContext";
 
 export default function Navbar() {
     const { account, connectWallet} = useWallet();
-    //const [panelInfo, setPanelInfo] =useState(false);
+    const [panelInfo, setPanelInfo] =useState(false);
 
-    /*
+    
     const dropdownPanel = (e) => {
         e.preventDefault();
         setPanelInfo(!panelInfo);
     };
-*/
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
@@ -25,7 +28,17 @@ export default function Navbar() {
                 
                 <ul className="navbar-menu">
                     <li className="navbar-item">
+                        {account ? (
                         <NavLink 
+                            to="/listYourProperty" 
+                            className={({ isActive }) => 
+                                `navbar-link ${isActive ? 'active' : ''}`
+                            }
+                        >
+                            <FaHome className="navbar-icon" />
+                            <span>List your property</span>
+                        </NavLink>
+                        ) : (<NavLink 
                             to="/home" 
                             className={({ isActive }) => 
                                 `navbar-link ${isActive ? 'active' : ''}`
@@ -34,6 +47,7 @@ export default function Navbar() {
                             <FaHome className="navbar-icon" />
                             <span>Home</span>
                         </NavLink>
+                    )}
                     </li>
 
                     <li className="navbar-item">
@@ -50,7 +64,30 @@ export default function Navbar() {
 
                     <li className="navbar-item">
                             {account ? (
-                                     <span className="navbar-button">Your account</span>
+                                <div className="container-wrapper-info"> 
+                                <div className="navbar-container-info"> 
+                                     <span onClick = {dropdownPanel} className="navbar-button">Your account</span>
+                                </div>
+
+                                {panelInfo && (
+                                    <div className="search-panel-info">
+                                        <NavLink to="/myAccount" className="navbar-link-option">
+                                            <FaUser className="navbar-icon"/>
+                                            <span>My Account</span>
+                                        </NavLink>    
+
+                                        <NavLink to="/myRentals" className="navbar-link-option">
+                                            <FaSuitcase className="navbar-icon" />
+                                            <span>My Rentals</span>
+                                        </NavLink>
+                                            
+                                        <NavLink to="/" className="navbar-link-option">
+                                            <RiLogoutCircleRFill className="navbar-icon"/>
+                                            <span>Logout</span>
+                                        </NavLink>  
+                                    </div>    
+                                )}
+                                </div>
                         ) : (<button type="button"
                             onClick={connectWallet} 
                             className="navbar-button">
