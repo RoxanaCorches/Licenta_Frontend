65bef7085b8f2e3f1cbd619b580322e6c65e044e
RoxanaCorches <roxanacorches032@gmail.com>
2026-02-03 15:06:54 +0200

    Upload Images Correct

--- src/components/listProperty/UploadImages.jsx
+++ src/components/listProperty/UploadImages.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { TbPhotoShare } from "react-icons/tb";
 
+
 export default function UploadImages(){
     const [mainImage, setMainImage] = useState(null);
-    const [otherImage, setOtherImage] = useState([]);
+    const [otherImage, setOtherImage] = useState([null, null, null, null]);
 
     const handleUploadeMainImage = (e) => {
         const image = e.target.files[0];
@@ -16,79 +17,91 @@ export default function UploadImages(){
         }
     };
 
-    const handleUploadeOtherImages = (e) => {
-        const images = Array.from(e.target.images);
-        const viewImages = images.map(image => ({
-            image,
-            preview: URL.createObjectURL(image)
-        }));
-        setOtherImage(prev =>  {
-            const max = [...prev, ...viewImages];
-            return max.splice(0,4);
-        });
+    
+    const handleUploadeOtherImages = (e, index) => {
+        const images = e.target.files[0];
+
+        if(!images) return;
+
+        const viewImages = [...otherImage];
+        viewImages[index] = {
+            image: images,
+            preview: URL.createObjectURL(images)
+        };
+        console.log(index,images);
+        setOtherImage(viewImages);
+
+        console.log(index,images);
     };
-
-
 
     return(
         <div className="form-section"> 
-                    <div className="container-upload-images-main"> 
-                       <snap className="content-image">Upload at least 5 photos of ypur property.</snap>
-                       <div className="upload-image-main"> 
-                            <TbPhotoShare className="icon-image-main"/>
-                            <div className="file-upload"> 
-                                <input
-                                    id="mainImageInput"
-                                    type="file"
-                                    multiple
-                                    accept="image/*"
-                                    onChange={handleUploadeMainImage}
-                                />
-                                <label htmlFor="mainImageInput" className="upload-button">Upload</label>
-                            </div>
-                        </div>
+            <div className="container-upload-images-main"> 
+                    <div className="upload-image-main"> 
+                        {mainImage &&  (
+                            <img
+                                className="view-image"
+                                src={mainImage.preview}
+                                alt={mainImage.image.name}
+                            />
+                        )}
 
-                        <div className="view-image">
-                            {mainImage &&  (
+                        {!mainImage && (
+                            <>
+                                <span className="content-image">Upload at least 5 photos of ypur property.</span>
+
+                                <TbPhotoShare className="icon-image-main"/>
+                                <div className="file-upload"> 
+                                    <input
+                                        id="mainImageInput"
+                                        type="file"
+                                        multiple
+                                        accept="image/*"
+                                        onChange={handleUploadeMainImage}
+                                    />
+                                    <label htmlFor="mainImageInput" className="upload-button">Upload</label>
+                                </div>
+                            </>
+                        )}
+                    </div>
+            </div>
+
+
+            <div className="container-others-images">   
+                {Array.from({ length: 4 }).map((_, containerIndex) => (   
+                    <div className="container-upload-images-next" key={containerIndex}> 
+
+                        <div className="upload-image-next"> 
+                            {otherImage[containerIndex] && (
                                 <img
-                                    
-                                    src={mainImage.preview}
-                                    alt={mainImage.image.name}
+                                    className="view-image"
+                                    src={otherImage[containerIndex].preview}
+                                    alt={otherImage[containerIndex].image.name}
+                                   
                                 />
                             )}
-                        </div>
-                    </div>
-                        
-                     <div className="container-others-images">   
-                    {Array.from({ length: 4 }).map((_, containerIndex) => (   
-                    <div className="container-upload-images-next"> 
-                       <div className="upload-image-next"> 
-                            <TbPhotoShare className="icon-image-next"/>
-                            <div className="file-upload"> 
-                                <input
-                                   id={`otherImagesInput-${containerIndex}`}
-                                    type="file"
-                                    multiple
-                                    accept="image/*"
-                                    onChange={handleUploadeOtherImages}
-                                />
-                                <label htmlFor="otherImagesInput" className="upload-button">Upload</label>
-                            </div>
-                        </div>
 
-                        <div className="view-image">
-                            {otherImage.map((img, index) => (
-                                <img
-                                    key={index}
-                                    src={img.preview}
-                                    alt={img.image.name}
-                                />
-                            ))}
+                           
+                          {!otherImage[containerIndex] && ( 
+                                <> 
+                                    <TbPhotoShare className="icon-image-next"/>
+                                    <div className="file-upload"> 
+                                        <input
+                                            id={`otherImagesInput-${containerIndex}`}
+                                            type="file"
+                                
+                                            accept="image/*"
+                                            onChange={(e) => handleUploadeOtherImages(e, containerIndex)}
+                                        />
+                                        <label htmlFor={`otherImagesInput-${containerIndex}`} className="upload-button">Upload</label>
+                                    </div>
+                                </>
+                            )}
+                            
                         </div>
                     </div>
-                    ))}
-                    </div>
+                ))}
+            </div>
         </div>
-        
     );
 }
